c35f6b20dfa214a852a74842336b42e90d6ca2d7
felix <felix@trufintech.io>
2024-10-28 13:22:48 +0000

    remove debug, as_ref

--- yaserde_derive/src/ser/expand_struct.rs
+++ yaserde_derive/src/ser/expand_struct.rs
@@ -160,16 +160,14 @@ pub fn serialize(
       }
 
       
-      println!("not cdata");
       let label_name = field.renamed_label(root_attributes);
       let conditions = condition_generator(&label, &field);
 
       if field.is_cdata() {
-        println!("cdata");
         return quote! {
             let start_event = ::yaserde::__xml::writer::XmlEvent::start_element(#label_name);
             writer.write(start_event).map_err(|e| e.to_string())?;
-            writer.write(::yaserde::__xml::writer::XmlEvent::characters(format!("<![CDATA[{}]]>", self.#label))).map_err(|e| e.to_string())?;
+            writer.write(::yaserde::__xml::writer::XmlEvent::characters(format!("<![CDATA[{}]]>", self.#label.as_str()))).map_err(|e| e.to_string())?;
             let end_event = ::yaserde::__xml::writer::XmlEvent::end_element();
             writer.write(end_event).map_err(|e| e.to_string())?;
         }.into()
